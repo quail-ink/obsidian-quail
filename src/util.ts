20917d4c64cfba3660d9f2b286f8a94c24b3f8b9
Jakey <jakey_yang@hotmail.com>
2023-07-12 15:30:23 +0800

    fix: markdown图片绝对路径与TFile.path 匹配
    插件库方法 app.vault.getFiles();  获取到的路径为 imgs/logo.png
    正则获取到正文的图片绝对路径为 /imgs/logo.png 
    
    两者不匹配导致无法触发图片上传流程

--- src/util.ts
+++ src/util.ts
@@ -87,7 +87,7 @@ export default {
       const imgPathMap :Record<string, any> = {};
       for (let ix = 0; ix < imgPathItems.length; ix++) {
         // @TODO: this is a hack, but it works for now
-        const formalizedPathname = decodeURIComponent(imgPathItems[ix]).replace(/^\.\//, '')
+        const formalizedPathname = decodeURIComponent(imgPathItems[ix]).replace(/^(\.\/|\/)/, '');
         imgPathMap[formalizedPathname] = {
           pathname: imgPathItems[ix],
           formalizedPathname,
